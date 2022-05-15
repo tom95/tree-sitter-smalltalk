3f55c0efda66682945baa9cd322927e0887314df
Tom Beckmann <tomjonabc@gmail.com>
2022-05-15 12:59:36 +0200

    add syntax highlighting for symbols

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,6 +1,7 @@
 [(self) (true) (false) (thisContext) (super) (nil)] @keyword
 (number) @number
 (string) @string
+(symbol) @string
 (identifier) @variable
 (unary_message (_) (identifier) @variable.part)
 (binary_operator) @variable.part
